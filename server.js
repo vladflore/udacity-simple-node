edfa3531f155eaa4cd525fe993dc2a2e25cdd69b
Vlad Flore <flore.vlad@gmail.com>
2021-01-06 10:59:14 +0100

    Log env var from code set prev as env var in travis

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ function sleep(ms) {
 }
 
 async function main() {
-  while(true) {
+  const favoriteFood = process.env.FAVORITE_FOOD;
+  console.log(`My favorite food is ${favoriteFood}`);
+  while (true) {
     console.log('Containers rule!');
     await sleep(5000);
   }
